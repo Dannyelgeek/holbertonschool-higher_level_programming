6c902aa6d4607ef5db3b8d7280e0d8e8865fa2fa
Dannyelgeek <daot1993@gmail.com>
2023-01-26 13:42:02 -0500

    Python: if/else, loops, functions task 12

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+for i in range(1, 101):
+    if i % 15 == 0:
+        print("fizz-buzz", end=' ')
+    elif i % 3 == 0:
+        print("fizz", end=' ')
+    elif i % 5 == 0:
+        print("buzz", end=' ')
+    else:
+        print(i, end=' ')
