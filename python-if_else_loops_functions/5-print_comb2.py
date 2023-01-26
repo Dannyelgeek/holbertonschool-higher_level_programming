f39303367bd4201b817cc5bec28139cbe12bbfe5
Dannyelgeek <daot1993@gmail.com>
2023-01-26 10:31:17 -0500

    Python: if/else, loops, functions task 5

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for nums in range(0, 100):
-    if nums < 98:
+    if nums <= 98:
         print(f'{nums:02d}, '.format(nums), end='')
     elif nums > 98:
         print('{}'.format(nums))
