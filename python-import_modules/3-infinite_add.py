01a5c96004995945aee77338b9358c23b024fddc
Dannyelgeek <daot1993@gmail.com>
2023-01-27 14:40:16 -0500

    Python: import & modules task 3

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    import sys
+
+    add = 0
+    num_args = len(sys.argv)
+    if sys.argv[0] == 1:
+        print(0)
+    else:
+        for ind in range(1, num_args):
+            add += int(sys.argv[ind])
+
+    print(add)
