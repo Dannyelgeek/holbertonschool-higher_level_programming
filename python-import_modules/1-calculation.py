2495ed8005da16a5502cfed15a84dc5c3db4d4b6
Dannyelgeek <daot1993@gmail.com>
2023-01-27 11:41:17 -0500

    Python: import & modules task 1

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,28 +1,9 @@
 #!/usr/bin/python3
-from calculator_1 import add
+import calculator_1
 if __name__ == '__main__':
     a = 10
     b = 5
-    result = add(a, b)
-    print('{} + {} = {}'.format(a, b, result))
-
-from calculator_1 import sub
-if __name__ == '__main__':
-    a = 10
-    b = 5
-    result = sub(a, b)
-    print('{} + {} = {}'.format(a, b, result))
-
-from calculator_1 import mul
-if __name__ == '__main__':
-    a = 10
-    b = 5
-    result = mul(a, b)
-    print('{} + {} = {}'.format(a, b, result))
-
-from calculator_1 import div
-if __name__ == '__main__':
-    a = 10
-    b = 5
-    result = div(a, b)
-    print('{} + {} = {}'.format(a, b, result))
+    print('{} + {} = {}'.format(a, b, calculator_1.add(a, b)))
+    print('{} - {} = {}'.format(a, b, calculator_1.sub(a, b)))
+    print('{} * {} = {}'.format(a, b, calculator_1.mul(a, b)))
+    print('{} / {} = {}'.format(a, b, calculator_1.div(a, b)))
