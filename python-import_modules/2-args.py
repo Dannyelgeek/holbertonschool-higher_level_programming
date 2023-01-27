d5ee76e2f4312f5e253392bb3adc34bbc524d65e
Dannyelgeek <daot1993@gmail.com>
2023-01-27 13:59:20 -0500

    Python: import & modules task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
     import sys
-    num_args = len(sys.argv) - 1
 
-    if num_args == 0:
+    count = len(sys.argv) - 1
+    if count == 0:
         print("0 arguments.")
-    elif num_args == 1:
-        print("1 argument.")
+    elif count == 1:
+        print("1 argument:")
     else:
-        print("{} arguments.".format(num_args))
-    for ind in range(num_args):
-        print("{}: {}".format(ind + 1, sys.argv[ind + 1]))
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
