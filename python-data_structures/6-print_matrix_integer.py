5730b5a89f31335286f485c1a5fc5904b7194240
Dannyelgeek <daot1993@gmail.com>
2023-01-31 16:34:40 -0500

    Python: data structures task 6.

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    '''
-    final_matrix = '\n'.join([' '.join(['{:d}'.format(item) for item in row]) for row in matrix])
-    print(final_matrix)
-    '''
     for ind in matrix:
         print(' '.join(map(str, ind)))
