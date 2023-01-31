ab1d81d36f14158430eb12c19cd52050537de61f
Dannyelgeek <daot1993@gmail.com>
2023-01-31 18:41:07 -0500

    Python: data structures task 8.

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    len_str = len(sentence)
-    first_chr = sentence[0]
-    if sentence[0] == "":
-        return len_str, None
+    if not sentence:
+        return 0, None
     else:
-        return len_str, first_chr 
+        len_str = len(sentence)
+        first_chr = sentence[0]
+        return len_str, first_chr
