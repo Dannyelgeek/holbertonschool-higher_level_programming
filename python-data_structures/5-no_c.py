e539f1b4733114c1e6ffaf1656d723e7a6715aec
Dannyelgeek <daot1993@gmail.com>
2023-01-31 15:30:54 -0500

    Python: data structures task 5.

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if my_string == None:
-        return
+    if my_string is None:
+        return None
     for ind in my_string:
         if ind == 'c':
             return my_string.translate({ord('c'): None})
