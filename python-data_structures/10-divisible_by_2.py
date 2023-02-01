29ff4c618a4126b79ec58e2f6523e6239d457f32
Dannyelgeek <daot1993@gmail.com>
2023-02-01 12:02:42 -0500

    Python: data structures task 10.

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    cpy_list = []
+    for nums in my_list:
+        if my_list[nums] % 2 == 0:
+            cpy_list.append(True)
+        else:
+            cpy_list.append(False)
+    return cpy_list
