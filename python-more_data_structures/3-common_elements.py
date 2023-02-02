ed12a37419656fbef9e5cd7275e4696e60dadd73
Dannyelgeek <daot1993@gmail.com>
2023-02-02 18:27:30 -0500

    Python: more data structures task 3.

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    for ind_1 in set_1:
-        for ind_2 in set_2:
-            if ind_1 == ind_2:
-                return ind_1
+    common_list = set(set_1).intersection(set_2)
+    return common_list
