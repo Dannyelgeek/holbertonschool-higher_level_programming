868f7b2b537ade64a96f33102bde27b1c1f2eb1c
Dannyelgeek <daot1993@gmail.com>
2023-02-02 16:09:24 -0500

    Python: more data structures activities.

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    cpy_matrix = matrix
+    arr_1 = []
+    for i in range(len(cpy_matrix)):
+        arr_2 = []
+        for x in cpy_matrix:
+            arr_2.append(x[i] ** 2)
+        arr_1.append(arr_2)
+    return arr_1
