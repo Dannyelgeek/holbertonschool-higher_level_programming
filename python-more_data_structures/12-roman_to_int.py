d903ad0a8f878a68d231b19ca88551921a76a5e6
Dannyelgeek <daot1993@gmail.com>
2023-02-03 12:54:06 -0500

    Python: more data structures task 12.

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,10 +2,9 @@
 def roman_to_int(roman_string):
     if not roman_string or type(roman_string) != str:
         return 0
-    roman_nums = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+    roman_nums = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     result = 0
     for ind in reversed(roman_string):
         traduce_nums = roman_nums[ind]
         result += traduce_nums if result < traduce_nums * 5 else -traduce_nums
     return result
-    
