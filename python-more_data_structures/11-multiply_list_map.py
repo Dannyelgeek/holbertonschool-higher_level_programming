e264853f99b8a25644503c009ceaf9f6c6884703
Dannyelgeek <daot1993@gmail.com>
2023-02-02 20:47:12 -0500

    Python: more data structures task 11.

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    cpy_list = my_list.copy()
-    mult_list = list(map(lambda x: x * number, cpy_list))
-    return mult_list
+    cpy_list =  list(map(lambda x: x * number, my_list))
+    return cpy_list
