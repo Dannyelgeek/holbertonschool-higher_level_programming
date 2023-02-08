abe465e4f3ba261f7a4bbc1d74a975239cb43808
Dannyelgeek <daot1993@gmail.com>
2023-02-07 19:38:33 -0500

    Python: classes task 2.

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -7,7 +7,7 @@ class Square:
     def __init__(self, size=0):
         try:
             self.__size = size
-        except size < 0:
+        except ValueError:
             raise ValueError("size must be >= 0")
-        except:
+        except TypeError:
             raise TypeError("size must be an integer")
