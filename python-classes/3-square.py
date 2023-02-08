189ad40c09e5085194eba4594e5892cdb4781de6
Dannyelgeek <daot1993@gmail.com>
2023-02-07 20:08:52 -0500

    Python: classes task 3.

---
+++ python-classes/3-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''Find the area in the Square class.'''
+
+
+class Square:
+    '''Return area of the square.'''
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        return self.__size * self.__size
