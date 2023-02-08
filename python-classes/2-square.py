c290d4052bc96f2eb076cb0344dcde9a6e600f68
Dannyelgeek <daot1993@gmail.com>
2023-02-07 19:36:17 -0500

    Python: classes task 2.

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -5,9 +5,9 @@
 class Square:
     '''Instatinate size with a value.'''
     def __init__(self, size=0):
-        if size == int:
+        try:
             self.__size = size
-        elif size < 0:
+        except size < 0:
             raise ValueError("size must be >= 0")
-        else:
+        except:
             raise TypeError("size must be an integer")
