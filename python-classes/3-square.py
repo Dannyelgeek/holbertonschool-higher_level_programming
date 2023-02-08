0a0bf213d67fc5a8a85260303111ca4ced62ea50
Dannyelgeek <daot1993@gmail.com>
2023-02-07 20:11:02 -0500

    Python: classes task 3.

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -3,7 +3,6 @@
 
 
 class Square:
-
     '''Return area of the square.'''
     def __init__(self, size=0):
         if not isinstance(size, int):
@@ -12,4 +11,5 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
     def area(self):
+
         return self.__size * self.__size
