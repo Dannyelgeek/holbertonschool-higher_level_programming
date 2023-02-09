6be4f0c36b448407da6c1a91ebe20c0ef982a07e
Dannyelgeek <daot1993@gmail.com>
2023-02-09 17:47:07 -0500

    Python: Test-driven development task 4.

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -3,23 +3,31 @@
 
 
 def text_indentation(text):
-    '''this function format a text using ., ? and :'''
+    """ prints "text" with 2 newlines after each of these char: ['.', '?', ':']
+    checks if "text" is a str
+    first loop removes spaces after each required chars
+    second loop adds 2 newlines after each required chars
+    """
     if type(text) != str:
         raise TypeError("text must be a string")
-    wildcards = [".", "?", ":"]
-    ind = 0
-    for chr in text:
-        if chr in wildcards:
-            if text[ind + 1] == " ":
-                text = text[:ind + 1] + text[ind + 2:]
-        else:
-            ind += 1
-    ind = 0
-    for chr in text:
-        if chr in wildcards:
-            text = text[:ind + 1] + "\n\n" + text[ind + 1:]
-            ind += 3
-        else:
-            ind += 1
+    toCatAfter = ['.', '?', ':']
 
-    print(text, end="")
+    # Removes the space after special chars
+    idx = 0
+    for items in text:
+        if items in toCatAfter:
+            if text[idx + 1] == " ":
+                text = text[:idx + 1] + text[idx + 2:]
+        else:
+            idx += 1
+
+    # Cats '\n\n' after the special char with removed space
+    idx = 0
+    for items in text:
+        if items in toCatAfter:
+            text = text[:idx + 1] + '\n\n' + text[idx + 1:]
+            idx += 3
+        else:
+            idx += 1
+
+    print(text, end='')
