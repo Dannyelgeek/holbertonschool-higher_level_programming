016412625e63968a22562d7c15c2d75ba87f5d67
Dannyelgeek <daot1993@gmail.com>
2023-02-10 13:31:26 -0500

    Python: more classes task 6.

---
+++ python-more_classes/6-rectangle.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python3
+'''creating a Rectangle.'''
+
+
+class Rectangle:
+    '''width will be a private attribute'''
+    number_of_instances = 0
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        '''Width attribute privacy.'''
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        '''Set width value.'''
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        '''height attribute privacy'''
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        '''Set height value.'''
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        '''Rectangle area.'''
+        area_w = self.__width
+        area_h = self.__height
+        return (area_w * area_h)
+
+    def perimeter(self):
+        '''Rectangle perimeter'''
+        per_w = self.__width
+        per_h = self.__height
+        if per_h == 0 or per_w == 0:
+            return 0
+        else:
+            return (2 * (per_w + per_h))
+
+    def __str__(self):
+        '''print a rectangle'''
+        new_line = ""
+        rect_w = self.__width
+        rect_h = self.__height
+        if rect_w == 0 or rect_h == 0:
+            return (new_line)
+        else:
+            for ind in range(rect_h):
+                if ind == rect_h - 1:
+                    new_line += ("#" * rect_w)
+                else:
+                    new_line += (("#" * rect_w) + "\n")
+        return (new_line)
+
+    def __repr__(self):
+        '''Return a representation of the rectangle'''
+        w = str(self.__width)
+        h = str(self.__height)
+        repr = "Rectangle(" + w + ", " + h + ")"
+        return (repr)
+
+    def __del__(self):
+        '''Prints a del message'''
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
