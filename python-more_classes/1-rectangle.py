63e863461005da770f28a3d5f80770e5e5a3385e
Dannyelgeek <daot1993@gmail.com>
2023-02-10 11:01:45 -0500

    Python: more classes task 1.

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -5,33 +5,33 @@
 class Rectangle:
     '''width will be a private attribute'''
     def __init__(self, width=0, height=0):
-        self.width == width
-        self.height == height 
+        self.width = width
+        self.height == height
 
     @property
     def width(self):
         '''Width attribute privacy.'''
         return (self.__width)
 
+    @width.setter
+    def width(self, value):
+        '''Set width value.'''
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+        
     @property
     def height(self):
         '''height attribute privacy'''
         return (self.__height)
 
-    @width.setter
-    def width(self, value):
-        '''Set width value.'''
-        if isinstance(value, int):
-            raise TypeError("width must be an integer")
-        elif value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width == value
-
     @height.setter
     def height(self, value):
         '''Set height value.'''
-        if isinstance(value, int):
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
