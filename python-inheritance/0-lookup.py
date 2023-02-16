bca753a582ce4008c3313b1f391368b2a116068c
Dannyelgeek <daot1993@gmail.com>
2023-02-16 08:22:18 -0500

    Python: Inheritance task 0.

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''This functuion returns the list of available attributes and methods of an object.'''
+
+
+def lookup(obj):
+    '''Return an object typied as a list.'''
+    return dir(obj)
