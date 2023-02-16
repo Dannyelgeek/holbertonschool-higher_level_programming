f668ca8dc435dd4efb6f50f834c5772bcae56f1d
Dannyelgeek <daot1993@gmail.com>
2023-02-16 10:13:46 -0500

    Python: Inheritance task 1.

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -4,6 +4,9 @@
 
 class MyList(list):
     '''Mylist is a subclass'''
+    def __init__(self):
+        '''Mylist constructor.'''
+        self.MyList
 
     def print_sorted(self):
         '''Module to sort the list'''
