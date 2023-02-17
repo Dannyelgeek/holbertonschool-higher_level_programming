a4fc4c5d9ea70e5ba1e62c5d3bf4de3d1288b31d
Dannyelgeek <daot1993@gmail.com>
2023-02-17 12:01:57 -0500

    Python: Input/Output task 5.

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -5,5 +5,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     '''Using a JSON representation'''
-    with open(filename, mode="w", encoding="UFT-8") as saveFile:
+    with open(filename, "w") as saveFile:
         json.dump(my_obj, saveFile)
