c53c8890a3048c3c48bed3b1f8a433785f730360
Dannyelgeek <daot1993@gmail.com>
2023-02-17 13:14:56 -0500

    Python: Input/Output task 12.

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''Create a pascal's triangle function.'''
+
+
+def pascal_triangle(n):
+    '''returns a list of lists of integers\
+         representing the Pascal's triangle.'''
+    if n <= 0:
+        return []
+    
+    triangles = [[1]]
+    while len(triangles) != n:
+        tri = triangles[-1]
+        temp = [1]
+        for ind in range(len(tri) - 1):
+            temp.append(tri[ind] + tri[ind + 1])
+        temp.append(1)
+        triangles.append(temp)
+    return triangles
