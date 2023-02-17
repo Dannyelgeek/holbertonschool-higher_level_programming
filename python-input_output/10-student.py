1f3ee908995ef18300adec5d6342bb5fb9c4d364
Dannyelgeek <daot1993@gmail.com>
2023-02-17 13:03:15 -0500

    Python: Input/Output task 10.

---
+++ python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''Write a class Student.'''
+
+
+class Student():
+    '''Define Student attributes.'''
+
+    def __init__(self, first_name, last_name, age):
+        "initialize a student"
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''Return a dict representation\
+             of the student'''
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}         
+        return self.__dict__
