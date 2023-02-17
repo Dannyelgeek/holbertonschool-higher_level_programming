badacd7a61737265e422d45057039caaa250442a
Dannyelgeek <daot1993@gmail.com>
2023-02-17 11:45:11 -0500

    Python: Input/Output task 1.

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -4,11 +4,5 @@
 
 def write_file(filename="", text=""):
     '''Return the numbers of chr written.'''
-    with open(filename) as readFile:
-        lines = 0
-        while True:
-            line = readFile.readline()
-            if not line:
-                break
-            lines += 1
-        return lines
+    with open(filename, "w", encoding="utf-8") as readFile:
+      return readFile.write(text)
