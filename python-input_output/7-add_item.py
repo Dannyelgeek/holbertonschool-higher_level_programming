abc882b8d078d72a565d7f287cc3b11791a4b922
Dannyelgeek <daot1993@gmail.com>
2023-02-17 12:34:26 -0500

    Python: Input/Output task 7.

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
 '''Adds all arguments to a Python list,\
      and then save them to a file'''
+import json
 import sys
-
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 try:
-    lst = load_from_json_file("add_item.json")
+    jsonList = load_from_json_file("add_item.json")
 except:
-    lst = []
+    jsonList = []
 
-argc = len(sys.argv)
-
-if argc > 1:
-    for i in range(1, argc):
-        lst.append(sys.argv[i])
-
-save_to_json_file(lst, "add_item.json")
+for i in sys.argv[1:]:
+    jsonList.append(i)
+save_to_json_file(jsonList, "add_item.json")
