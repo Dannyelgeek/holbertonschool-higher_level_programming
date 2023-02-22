157b401de94de93a8ae740eea7af962a1b727a2d
Dannyelgeek <daot1993@gmail.com>
2023-02-22 18:01:23 -0500

    Python: almost a circle task 11.

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -7,7 +7,24 @@ class Square(Rectangle):
     '''Square is a subclass'''
     def __init__(self, size, x=0, y=0, id=None):
         self.size = size
+        self.x = x
+        self.y = y
+        self.id = id
         super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        '''size private instance'''
+        return (self.__width)
+    
+    @size.setter
+    def size(self, value):
+        '''Set attribute value.'''
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
 
     def __str__(self) -> str:
         '''Overriding the square class'''
