d8010c7c0ba6c92b65486a5e51b9886d5697feec
Dannyelgeek <daot1993@gmail.com>
2023-02-21 20:55:55 -0500

    Python: almost a circle task 1.

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -6,7 +6,7 @@ class Base():
     '''Base class is a superclass.'''
     def __init__(self, id=None):
         self.__nb_objects = 0
-        if self.id != None:
+        self.id is not None:
             self.id = id
         else:
             self.id = self.__nb_objects += 1
