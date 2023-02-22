090b407cf0267a94cf99ace38ff6d432c649b9be
Dannyelgeek <daot1993@gmail.com>
2023-02-22 18:28:20 -0500

    Python: almost a circle task 12.

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -35,7 +35,7 @@ class Square(Rectangle):
         '''Assigns an argument to each attribute'''
         if args:
             len_args = len(args)
-            if len_args == 1:
+            if len_args == 1 or args[0]:
                 self.id = args[0]
             if len_args == 2:
                 self.size = args[1]
