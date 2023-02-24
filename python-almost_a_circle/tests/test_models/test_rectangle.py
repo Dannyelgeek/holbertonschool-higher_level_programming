c54b1749bac91c814fe6adf02771f014135536b4
Dannyelgeek <daot1993@gmail.com>
2023-02-24 13:49:05 -0500

    Python: almost a circle testing.

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 '''Testing Rectangle class.'''
 import unittest
+import os
+from io import StringIO
+from unittest.mock import patch
+from models.base import Base
 from models.rectangle import Rectangle
+from models.square import Square
 
 
 class TestBase(unittest.TestCase):
@@ -10,6 +15,14 @@ class TestBase(unittest.TestCase):
         '''Testing instances of the Rectangle class.'''
         t1 = Rectangle(1, 2)
         t2 = Rectangle(6, 9, 0, 0 ,12)
+        with self.assertRaises(TypeError):
+            t3 = Rectangle("string")
+            t4 = Rectangle(None)
+            t5 = Rectangle(float("inf"))
+            t6 = Rectangle(3.6, 9.1)
+            t7 = Rectangle(-8, 9)
+            t8 = Rectangle()
+
         self.assertEqual(t1.id, 18)
         self.assertEqual(t2.id, 12)
 
