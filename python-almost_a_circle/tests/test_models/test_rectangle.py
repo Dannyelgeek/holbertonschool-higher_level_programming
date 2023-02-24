c18a2c031dc6e33c31a42698a3f6ba5937095e5d
Dannyelgeek <daot1993@gmail.com>
2023-02-24 15:41:33 -0500

    Python: almost a circle testing.

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -36,7 +36,7 @@ class TestBase(unittest.TestCase):
             Rectangle(3, 6, 9, -12)
             raise ValueError()
 
-        self.assertEqual(t1.id, 20)
+        self.assertEqual(t1.id, 21)
         self.assertEqual(t2.id, 12)
 
     def test_area(self):
@@ -51,7 +51,29 @@ class TestBase(unittest.TestCase):
 
     def test_display(self):
         '''Testing test_display method'''
+        t6 = Rectangle(3, 2)
+        with patch('sys.stdout', new=StringIO()) as fakeOutput:
+            t6.display()
+            self.assertEqual(fakeOutput.getvalue(), '###\n###\n')
+
+        t7 = Rectangle(4, 5, 0, 1, 12)
+        with patch('sys.stdout', new=StringIO()) as fakeOutput:
+            t7.display()
+            self.assertEqual(fakeOutput.getvalue(),
+                             '\n####\n####\n####\n####\n####\n')
         
+    def test_str(self):
+        '''Testing ___str___ method'''
+        t8 = Rectangle(3, 2)
+        t9 = Rectangle(8, 7, 0, 0, 12)
+        t10 = Rectangle(3, 2, 1)
+        t11 = Rectangle(3, 2, id="holberton")
+
+        self.assertEqual(t8.__str__(), '[Rectangle] (22) 0/0 - 3/2')
+        self.assertEqual(t9.__str__(), '[Rectangle] (12) 0/0 - 8/7')
+        self.assertEqual(t10.__str__(), '[Rectangle] (23) 1/0 - 3/2')
+        self.assertEqual(t11.__str__(), '[Rectangle] (holberton) 0/0 - 3/2')
+
         
 
 if __name__ == "__main__":
