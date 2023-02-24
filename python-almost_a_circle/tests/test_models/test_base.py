fc1e50e193a097e0a9a793aace0063d04b6babe1
Dannyelgeek <daot1993@gmail.com>
2023-02-24 11:16:23 -0500

    Python: almost a circle test 1

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -6,10 +6,10 @@ from models.base import Base
 
 class TestBase(unittest.TestCase):
     '''Class for test Base class'''
-    def test_base_instance(self):
+    def test_init(self):
         '''Testing instances of th Base class.'''
         t1 = Base()
-        self.assertEqual(t1, 1)
+        self.assertEqual(t1.id, 1)
 
 if __name__ == "__main__":
     unittest.main()
