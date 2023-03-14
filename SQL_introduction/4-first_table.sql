989abf671a0be24822a2c167235c449b94f4985e
Dannyelgeek <daot1993@gmail.com>
2023-03-14 18:51:26 -0500

    SQL introduction task 4.

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,6 +1,6 @@
 -- creates a table called first_table in the current database in my MySQL server.
 USE hbtn_0c_0
-CREATE TABLE first_table (
-    id INT,
-    name VARCHAR(256)
+CREATE TABLE 'first_table' (
+    'id' INT,
+    'name' VARCHAR(256)
 );
