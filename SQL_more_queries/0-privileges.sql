02f3ec336233d5413ef00b5554aac56b62470b1d
Dannyelgeek <daot1993@gmail.com>
2023-03-16 08:58:36 -0500

    SQL more queries task 0.

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,2 +1,3 @@
 -- ists all privileges of the MySQL users user_0d_1 and user_0d_2 on my server.
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
