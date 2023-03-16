69e01f87d12d00b95f9580eee9c3ccd371ad36d4
Dannyelgeek <daot1993@gmail.com>
2023-03-16 16:07:40 -0500

    SQL more queries task 6.

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+-- creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on my MySQL server.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+    id INT UNIQUE AUTO_GENERATED NOT NULL PRIMARY KEY,
+    name VARCHAT(256) NOT NULL
+    );
