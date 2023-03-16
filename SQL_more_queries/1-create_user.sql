84fe10ed63a4aa79c3e58d8ee834976f72ee7f12
Dannyelgeek <daot1993@gmail.com>
2023-03-16 09:48:14 -0500

    SQL more queries task 1.

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- creates the MySQL server user user_0d_1.
-create user 'user_0d_1'@'localhost' IF NOT EXISTS IDENTIFY BY 'user_0d_1_pwd';
+create user 'user_0d_1'@'localhost' IF NOT EXISTS IDENTIFIED WITH authentication_plugin BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'
