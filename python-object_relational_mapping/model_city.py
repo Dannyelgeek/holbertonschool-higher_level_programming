214cd10ba6e23b5d185230569af248453b655a7f
Dannyelgeek <daot1993@gmail.com>
2023-03-17 16:49:59 -0500

    Python: object relational mapping task 14.

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,26 +1,13 @@
 #!/usr/bin/python3
 '''contains the class definition of a City.'''
 from sqlalchemy import ForeignKey, Column, Integer, String
-from sqlalchemy import MetaData
-from sqlalchemy.dialects import mysql
 from model_state import Base
 
 
 class City(Base):
     '''city class'''
     __tablename__ = 'cities'
-    charset = 'utf8'
-    id = Column(
-            mysql.INTEGER(11),
-            primary_key=True,
-            autoincrement=True,
-            nullable=False,
-            unique=True
-            )
+
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(
-            mysql.INTEGER(11),
-            ForeignKey("state.id"),
-            nullable=False
-            )
-    __table_args__ = {'mysql_charset ': ' latin1 '}
+    state_id = Column(Integer, ForeignKey('states.id'))
