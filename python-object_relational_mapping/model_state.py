4110918ef306d888d8a4ac161f77c24fea335e15
Dannyelgeek <daot1993@gmail.com>
2023-03-17 13:22:52 -0500

    Python: object relational mapping task 6.

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -9,6 +9,7 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 class State(Base):
+    'Creates State table.'
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, unique=True, autoincrement=True, nullable=False)
     name =  Column(String(158))
