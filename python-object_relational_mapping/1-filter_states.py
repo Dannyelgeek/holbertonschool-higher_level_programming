809cc73f6942690b01044b05b4fddd03f659e311
Dannyelgeek <daot1993@gmail.com>
2023-03-17 12:07:34 -0500

    Python: object relational mapping task 1.

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-'''lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa'''
+'''lists all states with a name starting with N 
+(upper N) from the database hbtn_0e_0_usa'''
 import MySQLdb
 import sys
 
@@ -12,7 +13,7 @@ if __name__ == '__main__':
     conn = MySQLdb.connect(host="localhost", port=3306, user=username,
                            passwd=password, db=database, charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    cur.execute("SELECT * FROM states WHERE UPPER(name) LIKE 'N%'")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
