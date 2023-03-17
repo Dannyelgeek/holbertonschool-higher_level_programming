22310e44fd68e5b9107e8060399fc40ba7af4cd4
Dannyelgeek <daot1993@gmail.com>
2023-03-17 16:57:30 -0500

    Python: object relational mapping task 14.

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,25 +1,24 @@
 #!/usr/bin/python3
-'''prints all City objects from the database hbtn_0e_14_usa'''
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
+""" Task 14 """
+
 from model_state import Base, State
 from model_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from sys import argv
 
-if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(username, password, database),
-                           pool_pre_ping=True)
-    
+if __name__ == '__main__':
+
+    engine = create_engine(
+            "mysql+mysqldb://{}:{}@localhost/{}".format(
+                argv[1],
+                argv[2],
+                argv[3]),
+            pool_pre_ping=True,
+            echo=False
+            )
+
     session = Session(engine)
-    rows = session.query(State, City).join(City).all()
-
-    for i in rows:
-        print("{}: ({}) {}".format(i[0].__dict__['name'],
-                                   i[1].__dict__['id'],
-                                   i[1].__dict__['name']))
-
-    session.close()
+    for state, city in session.query(State, City).filter(
+            City.state_id == State.id).order_by(City.id).all():
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
