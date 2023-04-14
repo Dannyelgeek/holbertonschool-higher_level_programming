6f73c90ea5847135686b6da1131f22fe86f380a1
Dannyelgeek <daot1993@gmail.com>
2023-04-13 20:45:10 -0500

    JavaScript: Warm up task 2.

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,11 +2,11 @@
 // prints a message depending of the number of arguments passed
 
 process.argv.forEach((val, ind) => {
-    if(ind === 1){
-        console.log('No argument');
-    } else if(ind === 2){
-        console.log('Argument found');
-    } else if(ind >= 3){
-        console.log('Arguments found');
-    }
+  if (ind === 1) {
+    console.log('No argument');
+  } else if (ind === 2) {
+    console.log('Argument found');
+  } else if (ind >= 3) {
+    console.log('Arguments found');
+  }
 });
