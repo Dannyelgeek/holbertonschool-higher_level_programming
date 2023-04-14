b7f3c587702deb9c34bbef931b508a5a06b4707b
Dannyelgeek <daot1993@gmail.com>
2023-04-14 14:48:11 -0500

    JavaScript: Warm up task 13.

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // returns the addition of 2 integers.
 
-function add(a, b) {
+exports.add = function() {
     numA = parseInt(a, 10);
     numB = parseInt(b, 10);
     return (numA + numB);
