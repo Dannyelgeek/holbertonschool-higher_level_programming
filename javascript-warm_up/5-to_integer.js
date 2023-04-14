e14e0f06e82e91498b682079ce24bbd304e80049
Dannyelgeek <daot1993@gmail.com>
2023-04-14 12:08:02 -0500

    JavaScript: Warm up task 5.

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -2,12 +2,12 @@
 // prints My number: <first argument converted in integer> if the first argument can be converted to an integer.
 
 const passedArg = process.argv.slice(2);
-let num = parseInt(passedArg, 10);
+const num = parseInt(passedArg, 10);
 
 if (!passedArg[0]) {
   console.log('Not a number');
 } else if (isNaN(num) === true) {
-    console.log('Not a number');
-} else if (typeof num === "number") {
-    console.log('My number:', num);
+  console.log('Not a number');
+} else if (typeof num === 'number') {
+  console.log('My number:', num);
 }
