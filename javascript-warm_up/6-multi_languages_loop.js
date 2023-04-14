1eaf5a23459542cf77aa36b9715730fc499b4ac3
Dannyelgeek <daot1993@gmail.com>
2023-04-14 12:21:49 -0500

    JavaScript: Warm up task 6.

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// prints 3 lines, but by using an array of string and a loop.
+
+const myArr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let ind = 0; ind < myArr.length; ind++) {
+  console.log(myArr[ind]);
+}
