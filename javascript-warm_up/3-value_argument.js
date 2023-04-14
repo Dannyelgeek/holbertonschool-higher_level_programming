c78bd98101fcdf98f31c2e773d56782899a11711
Dannyelgeek <daot1993@gmail.com>
2023-04-14 09:29:58 -0500

    JavaScript: Warm up task 3.

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
-//prints the first argument passed to it.
+// prints the first argument passed to it.
 
 const argsPassed = process.argv.slice(2);
 let argsBySpace = '';
 
 if (argsPassed.length === 0) {
-    console.log('No arguments');
+  console.log('No arguments');
 } else {
-    argsPassed.forEach((val ,ind) => {
-        argsBySpace += val + ' ';
-    });
-    console.log(argsBySpace);
+  argsPassed.forEach((val, ind) => {
+    argsBySpace += val + ' ';
+  });
+  console.log(argsBySpace);
 }
