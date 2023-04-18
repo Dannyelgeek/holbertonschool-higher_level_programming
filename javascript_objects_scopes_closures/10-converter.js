2f13150a76b5e9ecd85363f443722f462eb1ac91
Dannyelgeek <daot1993@gmail.com>
2023-04-18 18:37:24 -0500

    JavaScript: Objects, Scopes and Closures task 10.

--- javascript_objects_scopes_closures/10-converter.js
+++ javascript_objects_scopes_closures/10-converter.js
@@ -2,5 +2,7 @@
 //  converts a number from base 10 to another base passed as argument.
 
 exports.converter = function (base) {
-  return toString(base);
+  return function (number) {
+    return number.toString(base);
+  };
 };
