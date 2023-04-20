23c844af316ed9416222ba97b5779b7a41a547a0
Dannyelgeek <daot1993@gmail.com>
2023-04-20 12:10:25 -0500

    JavaScript: Web scraping task 2.

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -4,5 +4,8 @@ const passedArg = process.argv.slice(2);
 
 const request = require('request');
 request(passedArg[0], function (error, response, body) {
+  if (error) {
+    console.error(error);
+  }
   console.log('code:', response && response.statusCode);
 });
