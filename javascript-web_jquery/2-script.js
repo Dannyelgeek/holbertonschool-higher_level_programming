4d46becf0c4f22cac40d3dd35d14d977d1b51b83
Dannyelgeek <daot1993@gmail.com>
2023-04-21 09:01:54 -0500

    JavaScript: Web jQuery task 2.

---
+++ javascript-web_jquery/2-script.js
@@ -0,0 +1,5 @@
+// updates the text color of the <header> element to red (#FF0000) when the user clicks on the tag DIV#red_header.
+
+$('div').click(function(){
+    $('#red_header').css({'color': '#FF0000'});
+});
