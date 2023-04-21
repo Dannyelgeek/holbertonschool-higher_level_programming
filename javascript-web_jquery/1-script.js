dddc018e8ddc512d68c562153370c8cc7939adf2
Dannyelgeek <daot1993@gmail.com>
2023-04-21 08:50:47 -0500

    JavaScript: Web jQuery task 1.

--- javascript-web_jquery/1-script.js
+++ javascript-web_jquery/1-script.js
@@ -1,5 +1,3 @@
 // updates the text color of the <header> element to red (#FF0000)
 
-const header = document.querySelector('header');
-
-$(header).css({'color': '#FF0000'});
+$('header').css({'color': '#FF0000'});
