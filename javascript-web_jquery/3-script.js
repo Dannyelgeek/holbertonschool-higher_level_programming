08c3f717a7404ef37daf6ac51386b1f35f5c8b0b
Dannyelgeek <daot1993@gmail.com>
2023-04-21 10:21:09 -0500

    JavaScript: Web jQuery task 3.

--- javascript-web_jquery/3-script.js
+++ javascript-web_jquery/3-script.js
@@ -1,5 +1,5 @@
 // adds the class red to the <header> element when the user clicks on the tag DIV#red_header.
 
-$('div').click(function(){
-    $('header').addClass('red');
+$('div').click(function () {
+  $('header').addClass('red');
 });
