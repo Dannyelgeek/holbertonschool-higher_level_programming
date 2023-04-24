88170408018ec0772eeba223abe5779fe46467a6
Dannyelgeek <daot1993@gmail.com>
2023-04-24 16:21:45 -0500

    JavaScript: Web jQuery task 9.

--- javascript-web_jquery/9-script.js
+++ javascript-web_jquery/9-script.js
@@ -1,7 +1,7 @@
 /* fetches from https://stefanbohacek.com/hellosalut/?lang=fr and displays the value of hello
  from that fetch in the HTML tag DIV#hello.*/
 
-$.get('https://stefanbohacek.com/hellosalut/?lang=fr',
+$.get('https://hellosalut.stefanbohacek.dev/?lang=fr',
   { hello: '' }, function (data) {
     $('#hello').append(data.hello);
   });
